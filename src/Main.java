a0ea7025f7b0bd816b387162a63f2771f4b8640d
Leonardo Lima <leonardolimastos2003@gmail.com>
2026-05-05 20:31:12 -0300

    organizando e encapsulando classe filme

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,17 @@
+import br.com.screenmatch.models.Film;
+
 public class Main {
     public static void main(String[] args) {
-        Film myFilme = new Film();
-        myFilme.name = "Top Gun";
-        myFilme.releaseYear = 2002;
-        myFilme.durationInMinutes = 185;
+        Film myFilm = new Film();
+        myFilm.setName("Top Gun");
+        myFilm.setReleaseYear(2002);
+        myFilm.setDurationInMinutes(185);
 
-        myFilme.showTechnicalSheet();
-        myFilme.evaluate(10);
-        myFilme.evaluate(10);
-        myFilme.evaluate(8);
-        System.out.println(myFilme.sumEvaluations);
-        System.out.println(myFilme.totalEvaluations);
-        System.out.println(myFilme.getAvarage());
+        myFilm.showTechnicalSheet();
+        myFilm.evaluate(10);
+        myFilm.evaluate(10);
+        myFilm.evaluate(8);
+        System.out.println("Total de avaliações: " + myFilm.getTotalEvaluations());
+        System.out.println(myFilm.getAvarage());
     }
 }
