add8a0eb63aedb52e1a01fe6488994288895d165
Leonardo Lima <leonardolimastos2003@gmail.com>
2026-05-05 10:10:23 -0300

    implementando classe Film e seus atributos

---
+++ src/Film.java
@@ -0,0 +1,9 @@
+public class Film {
+    String nome;
+    int anoDeLancamento;
+    boolean incluidoNoPlano;
+    double avaliacao;
+    int totalDeAvaliacoes;
+    int duracaoEmMinutos;
+    
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Film myFilme = new Film();
+        myFilme.nome = "Top Gun";
+        myFilme.anoDeLancamento = 2002;
+        myFilme.duracaoEmMinutos = 185;
+
+        System.out.println(myFilme.nome);
+        System.out.println(myFilme.anoDeLancamento);
     }
 }
