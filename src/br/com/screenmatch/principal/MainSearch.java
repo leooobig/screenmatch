b90aad1c35f075f58ad100d9b331639975f562f3
leooobig <leonardolimastos2003@gmail.com>
2026-05-31 19:27:46 -0300

    adicionando apikey ao projeto com maven

--- src/br/com/screenmatch/principal/MainSearch.java
+++ src/br/com/screenmatch/principal/MainSearch.java
@@ -5,12 +5,16 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import io.github.cdimascio.dotenv.Dotenv;
 
 public class MainSearch {
     public static void main(String[] args) throws IOException, InterruptedException {
+        Dotenv dotenv = Dotenv.load();
+        String apiKey = dotenv.get("API_KEY");
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("")).build();
+                .uri(URI.create("https://www.omdbapi.com/?t=Interestelar&apikey="+apiKey)).build();
         HttpResponse<String> reponse = client.send(request, HttpResponse.BodyHandlers.ofString());
         System.out.println(reponse.body());
     }
