6aa44aa705db5eb138a9624ac55f68d4ccda3aa3
leooobig <leonardolimastos2003@gmail.com>
2026-06-08 08:14:48 -0300

    implementando busca dinâmica

--- src/br/com/screenmatch/principal/MainSearch.java
+++ src/br/com/screenmatch/principal/MainSearch.java
@@ -5,16 +5,25 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
+
 import io.github.cdimascio.dotenv.Dotenv;
 
 public class MainSearch {
     public static void main(String[] args) throws IOException, InterruptedException {
         Dotenv dotenv = Dotenv.load();
         String apiKey = dotenv.get("API_KEY");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite um filme para pesquisar: ");
+        var filmSearch = scanner.nextLine();
+        var treatedFilmSearch = filmSearch.trim().replace(" ", "+");
+        String url = "https://www.omdbapi.com/?t="+treatedFilmSearch+"&apikey="+apiKey;
+        System.out.println(treatedFilmSearch);
 
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://www.omdbapi.com/?t=Interestelar&apikey="+apiKey)).build();
+                .uri(URI.create(url)).build();
         HttpResponse<String> reponse = client.send(request, HttpResponse.BodyHandlers.ofString());
         System.out.println(reponse.body());
     }
