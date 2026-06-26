228a26b2f36fd03d1b5959c07044a4209be7b054
leooobig <leonardolimastos2003@gmail.com>
2026-06-25 21:06:19 -0300

    tratamento da busca com o URL Encoder

--- src/br/com/screenmatch/principal/MainSearch.java
+++ src/br/com/screenmatch/principal/MainSearch.java
@@ -2,9 +2,11 @@ package br.com.screenmatch.principal;
 
 import java.io.IOException;
 import java.net.URI;
+import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 import br.com.screenmatch.models.OmdbTittle;
@@ -12,6 +14,7 @@ import br.com.screenmatch.models.Tittle;
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.sun.source.tree.CompilationUnitTree;
 import io.github.cdimascio.dotenv.Dotenv;
 
 public class MainSearch {
@@ -20,22 +23,33 @@ public class MainSearch {
         String apiKey = dotenv.get("API_KEY");
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Digite um filme para pesquisar: ");
-        var filmSearch = scanner.nextLine();
-        var treatedFilmSearch = filmSearch.trim().replace(" ", "+");
-        String url = "https://www.omdbapi.com/?t="+treatedFilmSearch+"&apikey="+apiKey;
-        System.out.println(treatedFilmSearch);
+        var treatedFilmSearch = "";
+        while (treatedFilmSearch.isEmpty()) {
+            System.out.println("Digite um filme para pesquisar: ");
+            var filmSearch = scanner.nextLine();
+            treatedFilmSearch = URLEncoder.encode(filmSearch.trim(), StandardCharsets.UTF_8);
+            System.out.println(treatedFilmSearch);
+            if (treatedFilmSearch.isEmpty()){
+                System.out.println("A pesquisa não pode estar vazia, tente novemente: ");
+            }
+        }
 
+        String url = "https://www.omdbapi.com/?t=" + treatedFilmSearch + "&apikey=" + apiKey;
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(url)).build();
         HttpResponse<String> reponse = client.send(request, HttpResponse.BodyHandlers.ofString());
-
         String json = reponse.body();
+
         Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
         OmdbTittle omdbTittle = gson.fromJson(json, OmdbTittle.class);
-        Tittle myTittle = new Tittle(omdbTittle);
-        System.out.println(myTittle);
 
+        try{
+            Tittle myTittle = new Tittle(omdbTittle);
+            System.out.println(myTittle);
+        } catch (NumberFormatException e){
+            System.out.println("Erro na formatação da extração de dados");
+            System.out.println(e.getMessage());
+        }
     }
 }
